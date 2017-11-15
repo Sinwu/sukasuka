4e8eaedde7e4f138d78322a4beda1fae125cd522
Sinwu <wisnu.ru@gmail.com>
2017-11-15 18:44:11 +0700

    seed tbl user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+		'id' => 'fe472637-66a1-40f6-847f-d91d309da6ae',
+		'name' => 'Administrator',
+		'email' => 'admin.ediish@edi-indonesia.co.id',
+		'password' => '$2y$10$EPSNZtVOHX4OeoGLpw9mz.RVOG04HLLYq4p9u5Ci1CESFVePlm4/.',
+		'birthday' => '1991-11-11 00:00:00',
+		'active' => 1,
+		'admin' => 1,
+		'corporate' => 1,
+		'created_at' => '1991-11-11 00:00:00',
+		'updated_at' => '1991-11-11 00:00:00'
+	]);
+    }
+}
